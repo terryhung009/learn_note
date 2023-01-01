7bb23d227aeaf13f9b02fd0033a6dd24473e7516
DannyHung <76043300+Dannyhung009@users.noreply.github.com>
2023-01-02 01:23:55 +0800

    更新

---
+++ learn_nodejs/node/test/index.js
@@ -0,0 +1,5 @@
+var express =require("express");
+var app=express();
+app.listen(3000,function(){
+	console.log("伺服器已經啟動在 http://localhost:3000");
+});
---
+++ learn_php/php_oop1/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Customer{
+    public $id;
+    public $name;
+    public $email;
+    public $location;
+    public $balance;
+
+    public function getCustomwe(){
+        
+    }
+}
+
+// class Myclass extends Cutstomer{
+
+// }
