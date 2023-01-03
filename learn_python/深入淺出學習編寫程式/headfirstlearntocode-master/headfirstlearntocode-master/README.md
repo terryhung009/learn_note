2b7c3f6ceed4b9be851727ba173287f529acb9a6
DannyHung <khc741213@gmail.com>
2023-01-04 01:28:00 +0800

    更新

---
+++ learn_python/深入淺出學習編寫程式/headfirstlearntocode-master/headfirstlearntocode-master/README.md
@@ -0,0 +1,2 @@
+# Welcome to Head First Learn to Code
+You'll find all the source code from the book Head First Learn to Code here.
---
+++ learn_python/澎澎/code/code/List-Tuple.py
@@ -0,0 +1,31 @@
+#有序可變動列表 List
+grades=[12,15,70,90]
+# print(grades)
+
+#取出元素
+# print(grades[0])
+
+#存入元素
+# grades[0]=55
+# print(grades)
+
+#取得部分元素，包含前不包含後
+# print(grades[1:4])
+
+#連續刪除列表中從編號1到編號4(不包括)的資料
+# grades[1:4] = []
+# print(grades)
+
+#列表串接
+grades=[12,15,70,90]
+grades = grades+[33,44,55]
+print(grades)
+
+
+
+
+
+
+
+
+# 有序不可變動列表 Tuple
