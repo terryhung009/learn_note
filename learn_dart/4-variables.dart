1cc8fd824a250d74dc7b180acabe7bb30f39f40a
DannyHung <76043300+Dannyhung009@users.noreply.github.com>
2023-01-08 16:50:47 +0900

    更新

--- learn_dart/4-variables.dart
+++ learn_dart/4-variables.dart
@@ -1,6 +1,32 @@
 void main() {
   //弱類型
-// 申明變量 不初始值
+  // 申明變量 不初始值
+
+//var
+  var a;
+  // a = 'danny';
+  a = 123;
+  // a = false;
+  print(a);
+
+  //dynamic
+  // dynamic a = 'danny';
+  // a = 123;
+  // a = false;
+  // a.test();
+
+//Object
+  // Object b = 'danny';
+  // b = 123;
+  // b = false;
+  // b.test();
+
   //強類型
   //申明變量 並 初始值
+  // String a = 'danny';
+  // bool b = false;
+
+//一切對象的初始值都是null
+  // bool b;
+  // print(b);//報錯
 }
---
+++ learn_dart/5-constant.dart
@@ -0,0 +1,16 @@
+void main() {
+//定義
+  // final String a = 'danny';
+  // const String b = 'hung';
+  // final c = 'com';
+
+//不能修改
+  // a = 123;
+
+  //不能和 var 同用
+// final var = '123';
+
+//const賦值，必須用const 申明值
+  const List ls = const [123, 456, 789];
+  print(ls);
+}
